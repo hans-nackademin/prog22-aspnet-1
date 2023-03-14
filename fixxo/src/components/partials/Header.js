c364baa4279d3193875feb5734f13ba5b384a13d
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-03-14 13:59:29 +0100

    Lektion 1 (Design)

--- fixxo/src/components/partials/Header.js
+++ fixxo/src/components/partials/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header>
         <div className="container">
-            <Link className="logotype" to="/">Fixxo.</Link>
+            <Link id="logotype" to="/">Fixxo.</Link>
             
             <div id="menu">
                 <nav className="menu-links links">
@@ -15,11 +15,11 @@ const Header = () => {
                 </nav>
 
                 <nav className="menu-icons icons">
-                    <NavLink className="link"><i className="fa-regular fa-search"></i></NavLink>
-                    <NavLink className="link"><i className="fa-regular fa-code-compare"></i></NavLink>
-                    <NavLink className="link"><i className="fa-regular fa-heart"></i></NavLink>
-                    <NavLink className="link"><i className="fa-regular fa-bag-shopping"></i></NavLink>
-                    <NavLink className="link ms-5"><i className="fa-regular fa-user"></i></NavLink>
+                    <NavLink className="link" to="/search"><i className="fa-regular fa-search"></i></NavLink>
+                    <NavLink className="link" to="/compare"><i className="fa-regular fa-code-compare"></i></NavLink>
+                    <NavLink className="link" to="/favorites"><i className="fa-regular fa-heart"></i></NavLink>
+                    <NavLink className="link" to="/cart"><i className="fa-regular fa-bag-shopping"></i></NavLink>
+                    <NavLink className="link ms-5" to="/account"><i className="fa-regular fa-user"></i></NavLink>
                 </nav>
             </div>
 
