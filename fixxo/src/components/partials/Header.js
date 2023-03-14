8ec767dbe076303ceabda0cd30bb4cadca37b47e
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-03-14 11:50:21 +0100

    Lektion 1 (Design)

--- fixxo/src/components/partials/Header.js
+++ fixxo/src/components/partials/Header.js
@@ -5,24 +5,26 @@ const Header = () => {
   return (
     <header>
         <div className="container">
-            <Link to="/">Fixxo.</Link>
+            <Link className="logotype" to="/">Fixxo.</Link>
             
-            <nav>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/products">Products</NavLink>
-                <NavLink to="/contacts">Contacts</NavLink>
-            </nav>
+            <div id="menu">
+                <nav className="menu-links">
+                    <NavLink className="link" to="/">Home</NavLink>
+                    <NavLink className="link" to="/products">Products</NavLink>
+                    <NavLink className="link" to="/contacts">Contacts</NavLink>
+                </nav>
 
-            <nav>
-                <NavLink><i className="fa-regular fa-search"></i></NavLink>
-                <NavLink><i className="fa-regular fa-code-compare"></i></NavLink>
-                <NavLink><i className="fa-regular fa-heart"></i></NavLink>
-                <NavLink><i className="fa-regular fa-bag-shopping"></i></NavLink>
-                <NavLink><i className="fa-regular fa-user"></i></NavLink>
-            </nav>
+                <nav className="menu-icons icons">
+                    <NavLink className="link"><i className="fa-regular fa-search"></i></NavLink>
+                    <NavLink className="link"><i className="fa-regular fa-code-compare"></i></NavLink>
+                    <NavLink className="link"><i className="fa-regular fa-heart"></i></NavLink>
+                    <NavLink className="link"><i className="fa-regular fa-bag-shopping"></i></NavLink>
+                    <NavLink className="link ms-5"><i className="fa-regular fa-user"></i></NavLink>
+                </nav>
+            </div>
 
-            <div>
-                <button><i className="fa-regular fa-bars"></i></button>
+            <div id="toggle-icon" className="icons">
+                <button className="link"><i className="fa-regular fa-bars"></i></button>
             </div>
 
         </div>
