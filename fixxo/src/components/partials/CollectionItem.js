ea65ef8701581d0826444ef919095f9fbefdddff
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-03-14 16:02:31 +0100

    Lektion 1 (Design)

--- fixxo/src/components/partials/CollectionItem.js
+++ fixxo/src/components/partials/CollectionItem.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const CollectionItem = () => {
   return (
     <div className="item">
         <div className="image-section">
             <img src="https://kyhnet22sa.blob.core.windows.net/product-images/black-coat.png" alt="" />
+            <div className="icons">
+                <button className="link"><i className="fa-regular fa-code-compare"></i></button>
+                <button className="link"><i className="fa-regular fa-heart"></i></button>
+                <button className="link"><i className="fa-regular fa-bag-shopping"></i></button>
+            </div>
+            <NavLink className="btn-theme" to="/products">QUICK VIEW</NavLink>
+        </div>
+        <div className="body-section">
+            <p className="">d</p>
+            <p className="">d</p>
         </div>
     </div>
   )
