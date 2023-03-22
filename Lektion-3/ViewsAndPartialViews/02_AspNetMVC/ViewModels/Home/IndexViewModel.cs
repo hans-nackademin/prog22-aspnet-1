548febae801c014ccad01736f46185e2b97b9010
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-03-22 08:39:34 +0100

    blank

--- Lektion-3/ViewsAndPartialViews/02_AspNetMVC/ViewModels/Home/IndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using _02_AspNetMVC.Models;
-
-namespace _02_AspNetMVC.ViewModels.Home;
-
-public class IndexViewModel
-{
-    public CollectionModel BestCollection { get; set; } = null!;
-    public ShowcaseModel Showcase { get; set; } = null!;
-}
