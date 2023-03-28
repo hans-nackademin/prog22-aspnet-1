4dbfd189816e0b09025513204122964f6acd0eb5
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-03-28 13:39:42 +0200

    lektion 6 (forms & identity)

---
+++ lektion-6/WebApp_Identity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApp_Identity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
