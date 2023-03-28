2e541d1539009f5484d5614246a57a4c3a73a0f0
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-03-28 13:05:20 +0200

    lektion 6 (forms & identity)

--- lektion-6/WebApp_Forms.Shared/Models/User.cs
+++ lektion-6/WebApp_Forms.Shared/Models/User.cs
@@ -1,15 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿namespace WebApp_Forms.Shared.Models;
 
-namespace WebApp_Forms.Shared.Models
+public class User
 {
-    public class User
-    {
-        public int Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
-        public string? StreetName { get; set; }
-        public string? PostalCode { get; set; }
-        public string? City { get; set; }
-    }
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? StreetName { get; set; }
+    public string? PostalCode { get; set; }
+    public string? City { get; set; }
 }
--- lektion-6/WebApp_Forms/Program.cs
+++ lektion-6/WebApp_Forms/Program.cs
@@ -1,15 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using WebApp_Forms.Shared.Contexts;
+using WebApp_Forms.Shared.Repositories;
+using WebApp_Forms.Shared.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<DataContext>(x => 
-    x.UseSqlServer(builder.Configuration.GetConnectionString("Sql")));
-
-
-
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Sql")));
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<AddressRepository>();
+builder.Services.AddScoped<UserService>();
 
 var app = builder.Build();
 app.UseHttpsRedirection();
