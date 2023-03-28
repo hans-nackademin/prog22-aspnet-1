d43bb99a576a4398538a69fb42c970ae7878cdc2
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-03-28 09:30:21 +0200

    lektion 6 (forms & identity)

---
+++ lektion-6/WebApp_Forms/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Forms.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string something)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ lektion-6/WebApp_Forms/Models/Forms/UserRegistrationForm.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp_Forms.Models.Forms
+{
+    public class UserRegistrationForm
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
