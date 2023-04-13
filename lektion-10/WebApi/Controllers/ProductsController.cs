cf03e0f9f5812b23be86220efa8ec6338fdeed77
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-04-13 13:39:45 +0200

    lektion 10 (Web api)

---
+++ lektion-10/WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new List<string>
+            {
+                "Product 1", "Product 2", "Product 3"
+            });
+        }
+    }
+}
